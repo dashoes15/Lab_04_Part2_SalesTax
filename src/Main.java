0a2e3a70e5590fc8c08f6a5acd2f964a37b1fe08
dashoes15 <daltonasali@gmail.com>
2023-09-15 09:07:01 -0400

    Lab4 part2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double userValue;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Input price of purchase ");
+        userValue = scan.nextInt();
+        userValue += 0.05*userValue;
+        System.out.println("Your purchase with 5% sales tax is " + userValue + ".");
     }
 }
